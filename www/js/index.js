b020f9f10fce4808d0d918b912922b4b3c4c0d11
Luke Mitchell <mitl1_17@uni.worc.ac.uk>
2018-10-16 17:05:04 +0100

    Created App
    Added Index.html
    Added Index.js
    Added Config.xml
    Added JQuery Files

---
+++ www/js/index.js
@@ -0,0 +1,54 @@
+var paused_count =0;
+var resumed_count = 0;
+var launched_count = 0;
+
+var key = "firstname"
+var value = "Luke"
+
+document.addEventListener("deviceready", onDeviceReady, false);
+		
+	
+function updateDisplay() {
+	$("#launched").text("Application launched: " + launched_count);
+	$("#resumed").text("Application paused: " + paused_count);
+	$("#paused").text("Application resumed: " + resumed_count);
+}
+
+
+// device APIs are available
+//
+    function onDeviceReady() {
+	
+        document.addEventListener("resume", onResume, false);
+        document.addEventListener("pause", onPause, false);
+        
+        launched_count++;
+        updateDisplay();
+        
+        alert("device ready");
+        
+        
+        window.localstorage.setItem( key, value);
+        alert(window.localstorage.getItem(key));
+        
+        
+        
+    }
+
+
+    function onPause() {
+	
+	paused_count++;
+	updateDisplay();
+	    
+	alert("pause");
+    }
+	
+
+    function onResume() {
+		
+	resumed_count++;
+	updateDisplay();
+	    
+	alert("resume");
+    }
